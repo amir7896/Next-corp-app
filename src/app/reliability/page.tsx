9f1234588439d08f21be57df82e26c932c5e487a
amir7896 <79500734+amir7896@users.noreply.github.com>
2024-09-05 13:06:45 +0500

    Initial Commit

---
+++ src/app/reliability/page.tsx
@@ -0,0 +1,12 @@
+import reliabilityImg from "../../../public/reliability.jpg";
+import Hero from "@/components/hero";
+
+export default function ReliabilityPage() {
+  return (
+    <Hero
+      imgData={reliabilityImg}
+      imgAlt="welding"
+      title="Provide a good perfromance in web development."
+    />
+  );
+}
